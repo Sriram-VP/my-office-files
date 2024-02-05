3ab7cca8e5648c5fca708bd24fa29165356d4344
Sriramcogniot@gmail.com <Sriramcogniot@gmail.com>
2024-02-05 16:12:23 +0530

    header completed

--- Angular/skproject/src/app/mid-header/mid-header.component.ts
+++ Angular/skproject/src/app/mid-header/mid-header.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule, } from '@angular/material/form-field';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, NgForm, } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+export class MyErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: NgForm | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-mid-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatSelectModule, MatGridListModule, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './mid-header.component.html',
   styleUrl: './mid-header.component.scss'
 })
-export class MidHeaderComponent {
 
+export class MidHeaderComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 }
